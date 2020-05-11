a3c15cc615c670e6b8b66c677e49262728bea316
Simrandeep <singhsimrandeep19@gmail.com>
2020-05-11 23:09:09 +0530

    Designing the recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,18 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipe : Recipe[] = [
-    new Recipe(' A test recipe ', ' This is a test recipe ', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
-    
+  recipes : Recipe[] = [
+    new Recipe(' A test recipe ', ' This is a test recipe ', 
+    'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
+
+    new Recipe(' A test recipe 2 ', ' This is a test recipe ', 
+    'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
+
+
+    new Recipe(' A test recipe 3', ' This is a test recipe ', 
+    'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
+
+
   ];
   constructor() { }
 
