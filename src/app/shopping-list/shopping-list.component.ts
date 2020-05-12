6bccbb2f34acef8cd37e452ac8a81f1828d94aa7
Simrandeep <singhsimrandeep19@gmail.com>
2020-05-12 19:03:45 +0530

    Updated ingredient model and shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients : [];
+  ingredients : Ingredient[] = [];
 
   constructor() { }
 
