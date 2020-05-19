5a4cd8f1056ebe0dba7cc88faca363a56347445d
Simrandeep <singhsimrandeep19@gmail.com>
2020-05-19 18:28:24 +0530

    added functionality for add button in shopping component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,15 +9,20 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListComponent implements OnInit {
 
   ingredients : Ingredient[] = [
-    new Ingredient('chat masala ', 10),
-    new Ingredient('chat masala ', 10),
-    new Ingredient('chat masala ', 10),
+    new Ingredient('chat masala ', 5),
+    new Ingredient('sambhar masala ', 10),
+    new Ingredient('chane ka masala ', 3),
+    new Ingredient('pav bhaji masala ', 2),
 
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient : Ingredient){
+    this.ingredients.push(ingredient);
   }
 
 }
