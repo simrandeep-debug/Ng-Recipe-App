95a351a99959fcd553bd38ec9d60d9bc9279e690
Simrandeep <singhsimrandeep19@gmail.com>
2020-05-27 20:48:08 +0530

    Recipe service in progress

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,21 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
+    private recipes : Recipe[] = [
+        new Recipe(' A test recipe ', ' This is a test recipe ', 
+        'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
     
+        new Recipe(' A test recipe 2 ', ' This is a test recipe ', 
+        'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
+    
+    
+        new Recipe(' A test recipe 3', ' This is a test recipe ', 
+        'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
+    
+    
+      ];
+
+      getRecipes(){
+          return this.recipes.slice();
+      }
 }
