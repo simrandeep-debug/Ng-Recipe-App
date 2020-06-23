8152fb59191c8766e92908d4eed5fd9403073dd7
Simrandeep <singhsimrandeep19@gmail.com>
2020-06-23 22:04:04 +0530

    Child routing working now

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -21,6 +21,7 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit(){
     this.route.params.subscribe((params : Params) => {
         this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
     }
 
     );
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -56,6 +56,7 @@ export class RecipeService {
       addIngredientsToShoppingList(ingredients : Ingredient[]){
         this.shoppingListService.addIngredients(ingredients);
       }
+      
 
 
 }
